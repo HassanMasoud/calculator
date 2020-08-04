59da0288a68ab83feef5001af183e9ba594eba29
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-04 08:56:26 +0300

    Define calculator class and its methods

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+class Calculator {
+  constructor(previousOperandText, currentOperandText) {
+    this.previousOperandText = previousOperandText;
+    this.currentOperandText = currentOperandText;
+  }
+
+  clear() {}
+
+  delete() {}
+
+  appendNumber(number) {}
+
+  chooseOperation(operation) {}
+
+  compute() {}
+
+  updateDisplay() {}
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
